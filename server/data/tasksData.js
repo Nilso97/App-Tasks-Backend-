c94eca4abaa0f01849eb23d816d6b69ac8d80549
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2023-04-29 17:56:46 -0300

    refactor: adicionando arrow functions

--- server/data/tasksData.js
+++ server/data/tasksData.js
@@ -1,27 +1,27 @@
 const database = require("../infra/database");
 
-exports.getTasks = function () {
+exports.getTasks = () => {
   return database.query("SELECT * FROM app.tasks;");
 };
 
-exports.createTask = function (task) {
+exports.createTask = (task) => {
   return database.one(
     "INSERT INTO app.tasks (title, content, complete) VALUES ($1, $2, $3) RETURNING *;",
     [task.title, task.content, task.complete]
   );
 };
 
-exports.getTask = function (id) {
+exports.getTask = (id) => {
   return database.oneOrNone("SELECT * FROM app.tasks WHERE id = $1;", [id]);
 };
 
-exports.updateTask = function (id, task) {
+exports.updateTask = (id, task) => {
   return database.none(
     "UPDATE app.tasks SET title = $1, content = $2, complete = $3 WHERE id = $4;",
     [task.title, task.content, task.complete, id]
   );
 };
 
-exports.deleteTask = function (id) {
+exports.deleteTask = (id) => {
   return database.none("DELETE FROM app.tasks WHERE id = $1;", [id]);
 };
